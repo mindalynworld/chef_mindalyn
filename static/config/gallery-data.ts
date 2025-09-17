7ebce879d5b940c49d4a40244dbcb94c0a589dbf
Mindy <mindylynnng@gmail.com>
2025-09-17 16:22:56 -0700

    captions

--- static/config/gallery-data.ts
+++ static/config/gallery-data.ts
@@ -9,107 +9,107 @@ export const foodGalleryData: {
 {
     imgSrc: `${base}/assets/images/food/SweetSalmonBowl.jpeg`,
     alt: "",
-    caption: ""
+    caption: "honey soy sauce sesame salmon with seaweed & avocado"
 },
 {
     imgSrc: `${base}/assets/images/food/ChickenTortillaSoup.jpeg`,
     alt: "",
-    caption: ""
+    caption: "chicken tortilla soup"
 },
 {
     imgSrc: `${base}/assets/images/food/ChineseChickenWings.jpeg`,
     alt: "",
-    caption: ""
+    caption: "chinese chicken wings with broccolini"
 },
 {
     imgSrc: `${base}/assets/images/food/GreekYogurtEggs.jpeg`,
     alt: "",
-    caption: ""
+    caption: "sunny side up egg with greek yogurt + dill + lemon, with bacon and toast"
 },
 {
     imgSrc: `${base}/assets/images/food/MorocconTagine.jpeg`,
     alt: "",
-    caption: ""
+    caption: "moroccan chicken tagine with couscous"
 },
 {
     imgSrc: `${base}/assets/images/food/MusclePasta.jpeg`,
     alt: "",
-    caption: ""
+    caption: "muscle parsley pasta with asparagus"
 },
 {
     imgSrc: `${base}/assets/images/food/Oysters.jpeg`,
     alt: "",
-    caption: ""
+    caption: "fresh oysters from borough market in london"
 },
 {
     imgSrc: `${base}/assets/images/food/PadKraPow.jpeg`,
     alt: "",
-    caption: ""
+    caption: "thai pad kra pow (ground pork basil) topped with egg"
 },
 {
     imgSrc: `${base}/assets/images/food/PadSeeEw.jpeg`,
     alt: "",
-    caption: ""
+    caption: "thai pad see ew"
 },
 {
     imgSrc: `${base}/assets/images/food/RicottaSquash.jpeg`,
     alt: "",
-    caption: ""
+    caption: "summer squash and basil with ricotta on ciabatta toast"
 },
 {
     imgSrc: `${base}/assets/images/food/BreakfastSpread.jpeg`,
     alt: "",
-    caption: ""
+    caption: "breakfast spread: oats with fresh fruit, eggs, coffee"
 },
 {
     imgSrc: `${base}/assets/images/food/ChickenThighs.jpeg`,
     alt: "",
-    caption: ""
+    caption: "air fryer soy sauce chicken thighs with asparagus"
 },
 {
     imgSrc: `${base}/assets/images/food/7SalmonLemonPasta.jpeg`,
     alt: "",
-    caption: ""
+    caption: "baked lemon & salmon on top of tomato burrata pasta"
 },
 {
     imgSrc: `${base}/assets/images/food/10Fish.jpeg`,
     alt: "",
-    caption: ""
+    caption: "chinese steamed ginger fish"
 },
 {
     imgSrc: `${base}/assets/images/food/SardinePasta.jpeg`,
     alt: "",
-    caption: ""
+    caption: "sardine, tomato, olive, parsley pasta"
 },
 {
     imgSrc: `${base}/assets/images/food/Shakshuka.jpeg`,
     alt: "",
-    caption: ""
+    caption: "shakshuka with fresh sourdough made by my uncle"
 },
 {
     imgSrc: `${base}/assets/images/food/SpamEggAvoBowl.jpeg`,
     alt: "",
-    caption: ""
+    caption: "comfort food: spam egg avocado bowl with honey and soy sauce"
 },
 {
     imgSrc: `${base}/assets/images/food/SpinachPasta.jpeg`,
     alt: "",
-    caption: ""
+    caption: "spinach + basil + lemon + roasted garlic pasta"
 },
 {
     imgSrc: `${base}/assets/images/food/Sushi.jpeg`,
     alt: "",
-    caption: ""
+    caption: "sushi night"
 },
 {
     imgSrc: `${base}/assets/images/food/SweetPotatoeChickpea.jpeg`,
     alt: "",
-    caption: ""
+    caption: "chickpeas on bed of smashed sweet potato & kale, topped with chili sauce and tahini "
 },
 {
     imgSrc: `${base}/assets/images/food/ThanksgivingSpread1.jpeg`,
     alt: "",
-    caption: ""
+    caption: "braised chicken thighs in thanksgiving spread 2022"
 },
 {
     imgSrc: `${base}/assets/images/food/BeefBourguignon.jpeg`,
@@ -119,67 +119,67 @@ export const foodGalleryData: {
 {
     imgSrc: `${base}/assets/images/food/ThanksgivingSpread2.jpeg`,
     alt: "",
-    caption: ""
+    caption: "braised chicken thighs in thanksgiving spread 2023"
 },
 {
     imgSrc: `${base}/assets/images/food/Udon.jpeg`,
     alt: "",
-    caption: ""
+    caption: "udon with frozen dumplings"
 },
 {
     imgSrc: `${base}/assets/images/food/VietnameseVermicelli.jpeg`,
     alt: "",
-    caption: ""
+    caption: "vietnamese vermicelli bowl with lemongrass chicken"
 },
 {
     imgSrc: `${base}/assets/images/food/Zaalouk.jpeg`,
     alt: "",
-    caption: ""
+    caption: "moroccan zaalouk (cooked eggplant & tomato)"
 },
 {
     imgSrc: `${base}/assets/images/food/ZuchinniPancakes.jpeg`,
     alt: "",
-    caption: ""
+    caption: "zuchinni pancakes with green onion soy vinegar sauce"
 },
 {
     imgSrc: `${base}/assets/images/food/1PersianChicken.jpeg`,
     alt: "",
-    caption: ""
+    caption: "persian chicken with yellow rice and yogurt dip"
 },
 {
     imgSrc: `${base}/assets/images/food/2TaiwaneseChicken.jpeg`,
     alt: "",
-    caption: ""
+    caption: "tawianese basil chicken on rice"
 },
 {
     imgSrc: `${base}/assets/images/food/3Donuts.jpeg`,
     alt: "",
-    caption: ""
+    caption: "ube mochi donuts with ube glaze"
 },
 {
     imgSrc: `${base}/assets/images/food/4BreakfastTaco.jpeg`,
     alt: "",
-    caption: ""
+    caption: "breakfast tacos: cilantro garlic sauce, zuchinni, eggs, cheese"
 },
 {
     imgSrc: `${base}/assets/images/food/5KimchiFriedRice.jpeg`,
     alt: "",
-    caption: ""
+    caption: "kimchi fried rice"
 },
 {
     imgSrc: `${base}/assets/images/food/6Pasta.jpeg`,
     alt: "",
-    caption: ""
+    caption: "ingredients for burata tomato pasta"
 },
 {
     imgSrc: `${base}/assets/images/food/8TomatoSoup.jpeg`,
     alt: "",
-    caption: ""
+    caption: "tomato soup and grilled cheese"
 },
 {
     imgSrc: `${base}/assets/images/food/9Caprese.jpeg`,
     alt: "",
-    caption: ""
+    caption: "pesto pasta with caprese salad"
 },
 ];
 
@@ -281,26 +281,26 @@ export const craftGalleryData: {
 {
     imgSrc: `${base}/assets/images/crafts/saturn.jpeg`,
     alt: "",
-    caption: ""
+    caption: "saturn rug: tufted"
 },
 {
     imgSrc: `${base}/assets/images/crafts/roses.jpeg`,
     alt: "",
-    caption: ""
+    caption: "roses: water color and pen on paper"
 },
 {
     imgSrc: `${base}/assets/images/crafts/painting.jpeg`,
     alt: "",
-    caption: ""
+    caption: "acryclic on canvas"
 },
 {
     imgSrc: `${base}/assets/images/crafts/skirtshirt.jpeg`,
     alt: "",
-    caption: ""
+    caption: "top (sewed from scratch), skirt (thrift flipped from jeans)"
 },
 {
     imgSrc: `${base}/assets/images/crafts/fishies.jpeg`,
     alt: "",
-    caption: ""
+    caption: "koi fishies for popo: watercolor"
 },
 ];
