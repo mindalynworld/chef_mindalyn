622d7a5e40bfe08af457c6d3753547255699d6cf
mindalynworld <mindalynworld@users.noreply.github.com>
2025-09-17 21:02:26 +0000

    deploy: d6651677bfdc2f8890a6967c11461862a2160ec0

---
+++ lib/Components/Card.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let { imgSrc, alt, caption } = $props();
+</script>
+
+<main>
+    <img src={imgSrc} alt={alt}>
+</main>
+
+<style>
+    img {
+        width: 100%;
+    }
+</style>
