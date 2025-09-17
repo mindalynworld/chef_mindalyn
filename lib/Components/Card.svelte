78579e20afb211be571977e4821619e85391f099
mindalynworld <mindalynworld@users.noreply.github.com>
2025-09-17 23:23:56 +0000

    deploy: 7ebce879d5b940c49d4a40244dbcb94c0a589dbf

--- lib/Components/Card.svelte
+++ lib/Components/Card.svelte
@@ -3,11 +3,45 @@
 </script>
 
 <main>
-    <img src={imgSrc} alt={alt}>
+    <img class="image" src={imgSrc} alt={alt}>
+    {#if caption !== ""}
+        <div class="caption">
+            <p class="caption-text">{caption}</p>
+        </div>  
+    {/if}
 </main>
 
 <style>
-    img {
+    main {
+        position: relative;
         width: 100%;
+    }
+
+    .image {
+        width: 100%;
+        position: relative;
+    }
+
+    .caption {
+        position: absolute;
+        bottom: 0.25rem;
+        width: 94%;
+        font-size: 14px;
+        color: white;
+        background: linear-gradient(to bottom, transparent, black);
+        z-index: 100;
+        padding: 3%;
+        opacity: 0;
+    }
+
+    main:hover {
+        .caption {
+            opacity: 1;
+        } 
+    }
+
+    .caption-text {
+        margin-block-start: 2em;
+        margin-block-end: 0.5em;
     }
 </style>
